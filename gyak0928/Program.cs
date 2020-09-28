75d355609315972167882ccf54b7c4781ce6cc3f
Lakatos Balázs <lakatos.balazs2@vereb.dc>
2020-09-28 08:44:20 +0200

    Add project files.

---
+++ gyak0928/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak0928
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
